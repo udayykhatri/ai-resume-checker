59f48181b6b5554487d99f7a7b6d2e96a5c6844c
udayykhatri <udaykhatri65@gmail.com>
2026-06-21 23:00:51 +0530

    Update resume.js

--- backend_ai/Controllers/resume.js
+++ backend_ai/Controllers/resume.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { CohereClient } = require("cohere-ai");
 
 const cohere = new CohereClient({
-    token: ""yMdFTBggGhCPr6B2LE5oBJDnOIMKtoKOtK5xSHP2"", // 🔑 Paste your API key here
+    token: "yMdFTBggGhCPr6B2LE5oBJDnOIMKtoKOtK5xSHP2", // 🔑 Paste your API key here
 });
 
 
