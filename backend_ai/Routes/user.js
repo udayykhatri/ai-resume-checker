e329ddc3b19391cb912acd8858b0724b2f8a378e
udayykhatri <udaykhatri65@gmail.com>
2026-06-21 23:56:09 +0530

    Reorder routes in user.js for clarity

--- backend_ai/Routes/user.js
+++ backend_ai/Routes/user.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const UserController = require('../Controllers/user');
 
-router.post('/',UserController.register)
+router.get('/', (req, res) => {
+    res.send("User API Working");
+});
+
+router.post('/', UserController.register);
 
 module.exports = router;
